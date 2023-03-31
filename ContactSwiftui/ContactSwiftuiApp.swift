8d92876de8aa922bc8e2692dc3f5dec46cbcc86c
MTS <40478665@live.napier.ac.uk>
2023-04-01 05:39:29 +0630

    Initial Commit

---
+++ ContactSwiftui/ContactSwiftuiApp.swift
@@ -0,0 +1,20 @@
+//
+//  ContactSwiftuiApp.swift
+//  ContactSwiftui
+//
+//  Created by Myo Thandar soe on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactSwiftuiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
